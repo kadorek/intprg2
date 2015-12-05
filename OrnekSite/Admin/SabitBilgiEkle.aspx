552effb38fc77315bb4b190147578b0389c9f385
KadoreK <haltaskadir@gmail.com>
2015-12-05 19:59:27 +0200

    5-12-15

---
+++ OrnekSite/Admin/SabitBilgiEkle.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/Admin.Master" AutoEventWireup="true" CodeBehind="SabitBilgiEkle.aspx.cs" Inherits="OrnekSite.Admin.SabitBilgiEkle" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+
+    <div>
+       <b>kod:</b> <input type="text" runat="server" id="txtKod" />
+    </div>
+    <div>
+        <b>isim:</b> <input type="text" runat="server" id="txtIsim" />
+    </div>
+    <div>
+        <b>deger:</b> <input type="text" runat="server" id="txtDeger" />
+    </div>
+    <div>
+        <b>aktifmi:</b> <input type="checkbox" runat="server" id="chkAktifmi" />
+    </div>
+    <div>
+        <input type="submit" value="ekle" runat="server" id="btnEkle"  onserverclick="btnEkle_ServerClick"/>
+    </div> 
+</asp:Content>
---
+++ OrnekSite/Admin/SabitBilgiEkle.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OrnekSite.Admin
+{
+    public partial class SabitBilgiEkle : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnEkle_ServerClick(object sender,EventArgs e)
+        {
+            string kod = txtKod.Value;
+            string isim = txtIsim.Value;
+            string deger = txtDeger.Value;
+            bool durum = chkAktifmi.Checked;
+            Data.SabitBilgi sb = new Data.SabitBilgi(); //sabit bilgi türünden nesne oluşturduk referans tipli
+            sb.Kod = kod; // oluşturulan nesnenin kod özelliğine atandı
+            sb.Isim = isim; //  oluşturulan nesnenin isim özelliğine atandı
+            sb.Deger = deger;//  oluşturulan nesnenin değer özelliğine atandı
+            sb.AktifMi = durum;// oluşturulan nesnenin aktifmi özelliğine atandı
+
+            Data.VerilerDataContext db= new Data.VerilerDataContext();
+            db.SabitBilgis.InsertOnSubmit(sb);
+            db.SubmitChanges();
+
+
+
+
+
+
+        }
+    }
+}
